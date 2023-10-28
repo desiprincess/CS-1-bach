c39865a5dfdd38ccb28838b6618578401d3b2003
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-10-28 22:57:23 +0100

    Create assingment1.py

---
+++ assingment1.py
@@ -0,0 +1,30 @@
+# Crea una variable llamadda conejos y asignale el valor 126
+
+conejos = 126
+
+# Crea una variable llamada zanahorias y asígnale el valor 0
+
+zanahorias = 0
+
+# Muestra el contenido de la variable conejos
+
+print(conejos)
+
+# Modifica el valor de la variable conejos por 150. 
+
+conejos = 150 
+
+# Copia el valor de la variable conejos en la variable zanahorias. 
+
+zanahorias = conejos
+
+# Imprime el valor de las dos variables con print(). 
+# Aprendí a usar la "," en el print con stackoverflow.
+
+print(conejos , zanahorias)
+
+# Modifica el valor de conejos por 250 y vuelve a mostrar las dos variables.
+
+conejos = 250
+
+print(conejos , zanahorias)
