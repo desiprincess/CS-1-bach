c292ddb9411602365ab6f2da8f2ae5a00cc15d3f
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-10-28 23:29:12 +0100

    Create assingment2.py

---
+++ assingment2.py
@@ -0,0 +1,28 @@
+a = 3
+b = 6
+calc = a + b
+print(calc)
+
+a = a + 2
+b = b - 10
+calc = a + b
+
+print(calc)
+
+a += 1
+b += 7
+calc = a * b
+
+print(calc)
+
+a += 2
+b -= 1
+calc = a / b
+
+print(calc)
+
+a -= 1
+
+calc = a/b 
+
+print (calc)
