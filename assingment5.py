ec82163571f576905513d9b52fd4d023581e9eeb
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-10-29 00:05:05 +0100

    assingment5.py

---
+++ assingment5.py
@@ -0,0 +1,18 @@
+puntos = 50
+bono = 10
+puntos_totales = puntos + bono
+
+descuentos = 5
+precio_producto = 20 
+precio_final = precio_producto - (precio_producto * (descuentos * 0.01))
+
+print(puntos_totales)
+print(precio_final)
+
+bono = 15
+
+puntos_totales = puntos + bono
+precio_final = precio_producto - (precio_producto * (descuentos * 0.01))
+
+print (puntos_totales)
+print(precio_final)
