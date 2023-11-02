96983646db48929794bd99296331cd1094247319
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-11-02 20:49:45 +0000

    Create assingment13.py

---
+++ assingment13.py
@@ -0,0 +1,6 @@
+b1 = float(input("Por favor ingrese la longitud de la base menor del trapecio:"))
+b2 = float(input("Por favor ingrese la longitud de la base mayor del trapecio:"))
+h = float(input("Por favor ingrese la altura del trapecio:"))
+
+calc = 0.5 * (b1+b2) *h
+print("El área total del trapecio es:", calc)
