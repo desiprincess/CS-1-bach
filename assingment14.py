1bde327fdd6a180399d9e8496116df06a320d48e
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-11-08 12:31:32 +0000

    Create assingment14.py

---
+++ assingment14.py
@@ -0,0 +1,7 @@
+# Cómo usar "replace" 
+mensaje = "hola"
+print(mensaje.replace("h", "H"))
+
+# Cómo usar "extracción"
+mensaje = "Hola clase, ¿cómo están hoy? Espero que bien"
+print(mensaje [0:-15])
