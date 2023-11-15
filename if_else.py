037f63979c426b704265f031d88433069583ca29
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-11-15 12:30:37 +0000

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,15 @@
+# centencias condicionales: if. 
+# if condición : (indica un "si pasa esto")
+#   instrucción 
+
+edad = int (input("Dime tu edad"))
+if edad >= 18:
+    print("Eres mayor de edad")
+#else indica otra cosa, es como un "y sino". 
+else: 
+    print ("Eres menor de edad")
+# operadores: 
+# igual : ==
+# mayor que > ; mayor igual >=
+# menor que < ; menos igual <=
+# no igual a (diferente) : !=
