835b8947279f398b16e90d54fbfa8ff5a6b0442c
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-11-15 12:41:11 +0000

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,11 @@
+# Condicionales anidadas - programa que convierte nº a letras.
+número = int(input("Di un número que quieras convertir"))
+if número == 1: 
+    print("El número es igual a uno")
+# si quiero seguir dándole isntrucciones a la misma variable, se usa "elif".
+elif número ==3: 
+    print("El número es igual a dos")
+elif número ==3: 
+    print ("El número es igual a tres")
+else: 
+    print ("Este programa solo convierte el 1, 2 y 3.")
