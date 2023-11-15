b39c9acf7b5f81323300b826c1f6116cfb93ad9d
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-11-15 13:07:43 +0000

    Create if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,7 @@
+precio = int(input("Introduce el precio del hotel"))
+if precio <=50:
+    print ("El hotel es barato")
+elif precio <=100: 
+    print ("El hotel tiene un precio normal")
+else:
+    print ("El hotel es caro")
