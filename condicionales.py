e8636dca15b2a4d3eea56e6c4aaaf7d4c44e0342
Vipasha Pahilwani <149272929+desiprincess@users.noreply.github.com>
2023-11-22 12:32:17 +0000

    Create condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,14 @@
+print ("Menú del dia:")
+print ("1. Ensalada")
+print ("2. Pizza")
+print ("3. Hamburguesa")
+elección = int(input("Introduzca el número de su elección"))
+
+if elección == 1:
+    print("Ha elegido la ensalada")
+elif elección == 2:
+    print("Ha eledigo la pizza")
+elif elección == 3: 
+    print("Ha elegido la hamburguesa")
+else:
+    print ("Esa opción no se encuentra en el menú")
